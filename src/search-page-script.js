ef83786b53eab13d18f3b61d6994ca4894b15e04
CalShark <callumshark@yahoo.com>
2022-11-06 13:15:21 -0500

    testing script for search-page

---
+++ src/search-page-script.js
@@ -0,0 +1,8 @@
+const searchButton = document.querySelector(".search-btn");
+
+searchButton.addEventListener('click', ()=> {
+    console.log("search button clicked")
+});
+
+//fetch('http://localhost:8080/api/properties')
+//    .then
